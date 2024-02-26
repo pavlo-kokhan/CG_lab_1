320fecb7ab91e0f1015544f18c80b809d62c73ab
pavlo-kokhan <pavlokokhan@gmail.com>
2024-02-26 15:43:33 +0200

    normal axes, grid, vertexes

--- script.js
+++ script.js
@@ -1,67 +1,146 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const container = document.querySelector('.canvas-container');
-canvas.width = container.clientWidth;
-canvas.height = container.clientHeight;
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+const container = document.querySelector('.canvas-container')
+canvas.width = container.clientWidth
+canvas.height = container.clientHeight
 
-drawGrid(canvas.width, canvas.height);
+const scale = 20
 
-function drawTriangle() {
-    const x = parseInt(document.getElementById('xCoord').value);
-    const y = parseInt(document.getElementById('yCoord').value);
-    const length = parseInt(document.getElementById('sideLength').value);
-    const selectedColor = document.getElementById('colorSelector').value;
-    const vertexShape = document.getElementById('shape').value;
+function setDefaultStrokeStyle(color, lineWidth) {
+    context.strokeStyle = color
+    context.lineWidth = lineWidth
+}
 
-    const height = Math.sqrt(3) / 2 * length;
-    const x2 = x - length / 2;
-    const y2 = y + height;
-    const x3 = x + length / 2;
-    const y3 = y + height;
+function drawCoordinateAxes(width, height, step, stripLength) {
+    context.beginPath()
+    
+    context.moveTo(0, height / 2)
+    context.lineTo(width, height / 2)
+
+    context.moveTo(width / 2, 0)
+    context.lineTo(width / 2, height)
+
+    for (let x = width / 2 + step; x <= width; x += step) {
+        context.moveTo(x, height / 2 - stripLength / 2)
+        context.lineTo(x, height / 2 + stripLength / 2)
+    }
+
+    for (let x = width / 2 - step; x >= 0; x -= step) {
+        context.moveTo(x, height / 2 - stripLength / 2)
+        context.lineTo(x, height / 2 + stripLength / 2)
+    }
+
+    for (let y = height / 2 + step; y <= height; y += step) {
+        context.moveTo(width / 2 - stripLength / 2, y)
+        context.lineTo(width / 2 + stripLength / 2, y)
+    }
+
+    for (let y = height / 2 - step; y >= 0; y -= step) {
+        context.moveTo(width / 2 - stripLength / 2, y)
+        context.lineTo(width / 2 + stripLength / 2, y)
+    }
+
+    context.stroke()
+}
+
+function drawGrid(width, height, step) {
+    context.beginPath()
+
+    for (let y = height / 2 + step; y <= height; y += step) {
+        context.moveTo(0, y);
+        context.lineTo(width, y);
+        context.moveTo(0, height / 2 - (y - height / 2));
+        context.lineTo(width, height / 2 - (y - height / 2));
+    }
+    
+    for (let x = width / 2 + step; x <= width; x += step) {
+        context.moveTo(x, 0);
+        context.lineTo(x, height);
+        context.moveTo(width / 2 - (x - width / 2), 0);
+        context.lineTo(width / 2 - (x - width / 2), height);
+    }
+
+    setDefaultStrokeStyle('gray', 0.25)
+    context.stroke()
+
+    setDefaultStrokeStyle('black', 1)
+}
+
+function drawRectangleVertex(centerX, centerY, width, height, color) {
+    context.beginPath()
+
+    context.moveTo(centerX - width / 2, centerY + height / 2)
+    context.lineTo(centerX - width / 2, centerY - height / 2)
+    context.lineTo(centerX + width / 2, centerY - height / 2)
+    context.lineTo(centerX + width / 2, centerY + height / 2)
+    context.closePath()
+
+    context.fillStyle = color
+    context.fill()
+    context.stroke()
+}
+
+function drawCircleVertex(x, y, radius, color) {
+    context.beginPath()
+    context.arc(x, y, radius, 0, Math.PI * 2)
+
+    context.fillStyle = color
+    context.fill()
+    context.stroke()
+}
+
+function drawTriangle(x, y, length, vertexShape, selectedColor) {
+    const height = Math.sqrt(3) / 2 * length
+    const x2 = x - length / 2
+    const y2 = y + height
+    const x3 = x + length / 2
+
+    // заборона малювати поза межами canvas
+    if (y < 0 || y2 > canvas.height || x2 < 0 || x3 > canvas.width) return
  
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x2, y2);
-    ctx.lineTo(x3, y3);
-    ctx.fillStyle = selectedColor;
-    ctx.fill();
+    context.beginPath()
+
+    context.moveTo(x, y)
+    context.lineTo(x2, y2)
+    context.lineTo(x3, y2)
+    context.closePath()
+
+    context.stroke()
+    context.fillStyle = selectedColor
+    context.fill()
 
     if (vertexShape === 'square') {
-        ctx.fillRect(x, y, 5, 5);
-        ctx.fillRect(x2, y2, 5, 5);
-        ctx.fillRect(x3, y3, 5, 5);
-    } 
-    else if (vertexShape === 'circle') {
-        ctx.beginPath();
-        ctx.arc(x, y, 3, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(x2, y2, 3, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(x3, y3, 3, 0, Math.PI * 2);
-        ctx.fill();
+        drawRectangleVertex(x, y, 5, 5)
+        drawRectangleVertex(x2, y2, 5, 5)
+        drawRectangleVertex(x3, y2, 5, 5)
+
+    } else if (vertexShape === 'circle') {
+        drawCircleVertex(x, y, 3)
+        drawCircleVertex(x2, y2, 3)
+        drawCircleVertex(x3, y2, 3)
     }
 }
 
-function drawGrid(width, height) {
-    ctx.beginPath();
-    const step = 20;
+const btnDraw = document.querySelector(".btn-draw")
+const btnClear = document.querySelector(".btn-clear")
 
-    for (let x = 0; x <= width; x += step) {
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, height);
-    }
+setDefaultStrokeStyle('black', 1)
+drawCoordinateAxes(canvas.width, canvas.height, scale, 8)
+drawGrid(canvas.width, canvas.height, scale)
 
-    for (let y = 0; y <= height; y += step) {
-        ctx.moveTo(0, y);
-        ctx.lineTo(width, y);
-    }
+btnDraw.onclick = () => {
+    const x = parseInt(document.getElementById('x-coord').value) * scale + canvas.width / 2
+    const y = parseInt(document.getElementById('y-coord').value) * scale * (-1) + canvas.height / 2
+    const length = parseInt(document.getElementById('side-length').value) * scale
+    const vertexShape = document.getElementById('shape').value
+    const selectedColor = document.getElementById('triangle-color').value
 
-    ctx.strokeStyle = 'gray';
-    ctx.stroke();
+    // drawTriangle(200, 300, 100, 'circle', 'black')
+    drawTriangle(x, y, length, vertexShape, selectedColor);
 }
 
-function clear() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+btnClear.onclick = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    drawCoordinateAxes(canvas.width, canvas.height, scale, 8)
+    drawGrid(canvas.width, canvas.height, scale)
 }
